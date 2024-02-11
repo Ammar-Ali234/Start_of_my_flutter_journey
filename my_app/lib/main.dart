cb5b71cb31e85badcaa539aa02ea9a825eb88abd
Ammar-Ali234 <ammaralif21@nutech.edu.pk>
2024-02-11 12:53:40 +0500

    Day2 completed

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/home_page.dart';
 
 void main() {
   runApp(Myapp());
@@ -9,12 +10,6 @@ class Myapp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Material(
-      child: Center(
-          child: Container(
-        child: Text("hello!"),
-      )),
-    ));
+    return MaterialApp(home: HomePage());
   }
 }
